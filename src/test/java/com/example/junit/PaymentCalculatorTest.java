7d2cb09708dac2a83419a8c5e4724bbf09e82cc1
valentin-shipilov <valentin.shipilov@gmail.com>
2021-12-14 13:48:51 +0300

    Update PaymentCalculatorTest.java

--- src/test/java/com/example/junit/PaymentCalculatorTest.java
+++ src/test/java/com/example/junit/PaymentCalculatorTest.java
@@ -3,7 +3,9 @@ package com.example.junit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PaymentCalculatorTest {
-    private final PaymentCalculator testee = new PaymentCalculator(100);
+    private static final long givenCostForMinute = 100;
 
+    private final PaymentCalculator testee = new PaymentCalculator(givenCostForMinute);
+    
     // Задание: добавить тесты для метода calculatePayment
 }
